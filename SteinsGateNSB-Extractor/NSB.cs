4916d7fbb07638be58aeeec5cb31cd9f476a797f
lehieugch68 <lehieuvnua68@gmail.com>
2020-11-19 00:02:55 +0700

    Initial commit

---
+++ SteinsGateNSB-Extractor/NSB.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SteinsGateNSB_Extractor
+{
+    public class NSB
+    {
+        public static List<string> Unpack(string input)
+        {
+            List<string> result = new List<string>();
+            BinaryReader reader = new BinaryReader(File.OpenRead(input));
+            while (reader.BaseStream.Position < reader.BaseStream.Length)
+            {
+                int index = reader.ReadInt32();
+                short unk = reader.ReadInt16();
+                short strCount = reader.ReadInt16();
+                if (strCount == 0) continue;
+                for (int i = 0; i < strCount; i++)
+                {
+                    int strLen = reader.ReadInt32();
+                    string str = Encoding.Unicode.GetString(reader.ReadBytes(strLen));
+                    if (!str.StartsWith("<PRE")) continue;
+                    result.Add($"/*{index}-{i}*/");
+                    result.Add(str);
+                }
+            }
+            reader.Close();
+            return result;
+        }
+
+        public static byte[] Repack(string txtFile, string nsbFile)
+        {
+            MemoryStream result = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(result);
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            StreamReader txtReader = new StreamReader(File.OpenRead(txtFile));
+            string line = string.Empty;
+            while (!txtReader.EndOfStream)
+            {
+                if (line.StartsWith("/*") && line.EndsWith("*/"))
+                {
+                    string key = line;
+                    List<string> str = new List<string>();
+                    try
+                    {
+                        while (!(line = txtReader.ReadLine()).StartsWith("/*"))
+                        {
+                            str.Add(line);
+                        }
+                    }
+                    catch { }
+                    dict.Add(key, string.Join("\n", str.ToArray()));
+                }
+                else
+                {
+                    line = txtReader.ReadLine();
+                }    
+            }
+            txtReader.Close();
+            BinaryReader nsbReader = new BinaryReader(File.OpenRead(nsbFile));
+            while (nsbReader.BaseStream.Position < nsbReader.BaseStream.Length)
+            {
+                int index = nsbReader.ReadInt32();
+                short unk = nsbReader.ReadInt16();
+                short strCount = nsbReader.ReadInt16();
+                writer.Write(index);
+                writer.Write(unk);
+                writer.Write(strCount);
+                for (int i = 0; i < strCount; i++)
+                {
+                    int strLen = nsbReader.ReadInt32();
+                    byte[] strBytes = nsbReader.ReadBytes(strLen);
+                    string newStr;
+                    if (dict.TryGetValue($"/*{index}-{i}*/", out newStr))
+                    {
+                        byte[] utf8Bytes = Encoding.UTF8.GetBytes(newStr);
+                        byte[] utf16Bytes = Encoding.Convert(Encoding.UTF8, Encoding.Unicode, utf8Bytes);
+                        writer.Write(utf16Bytes.Length);
+                        writer.Write(utf16Bytes);
+                    }
+                    else
+                    {
+                        writer.Write(strLen);
+                        writer.Write(strBytes);
+                    }                        
+                }
+            }
+            nsbReader.Close();
+            writer.Close();
+            return result.ToArray();
+        }
+    }
+}
---
+++ SteinsGateNSB-Extractor/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SteinsGateNSB_Extractor
+{
+    class Program
+    {
+        private const string _NSBExt = ".nsb";
+        private const string _TextExt = ".txt";
+        static void Main(string[] args)
+        {
+            Console.Title = "SteinsGate NSB Extractor by LeHieu - VietHoaGame";
+            if (args.Length > 0)
+            {
+                foreach (string input in args)
+                {
+                    string ext = Path.GetExtension(input).ToLower();
+                    string name = Path.GetFileName(input);
+                    switch (ext)
+                    {
+                        case _NSBExt:
+                            List<string> output = NSB.Unpack(input);
+                            string outFile = Path.Combine(Path.GetDirectoryName(input), $"{Path.GetFileNameWithoutExtension(input)}.{_TextExt.TrimStart((char)46)}");
+                            File.WriteAllLines(outFile, output.ToArray());
+                            Console.WriteLine($"Unpacked: {name} -> {Path.GetFileName(outFile)}");
+                            break;
+                        case _TextExt:
+                            string nsbFile = Path.Combine(Path.GetDirectoryName(input), $"{Path.GetFileNameWithoutExtension(input)}.{_NSBExt.TrimStart((char)46)}");
+                            if (!File.Exists(nsbFile))
+                            {
+                                Console.WriteLine($"Skip: {name}. Reason: NSB file not found.");
+                            }
+                            else
+                            {
+                                byte[] bytes = NSB.Repack(input, nsbFile);
+                                File.WriteAllBytes($"{nsbFile}", bytes);
+                                Console.WriteLine($"Repacked: {name} -> {Path.GetFileName(nsbFile)}");
+                            }    
+                            break;
+                        default:
+                            Console.WriteLine($"Skip: {name}. Reason: The file format is not supported.");
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please drag and drop files into this tool to unpack/repack.");
+            }    
+            Console.ReadKey();
+        }
+    }
+}
